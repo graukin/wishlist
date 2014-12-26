abebb89e31755c5894c4c9d0fa1423d467017ab2
Takesha <graukin>
2014-12-26 23:56:37 +0300

    work with command line; interactive update

--- src/wishlist.pl
+++ src/wishlist.pl
@@ -2,19 +2,36 @@
 
 use WishList;
 
-while (1) {
-  print '>> ';
-  chomp($_ = <>);
+print '>> ';
+while (<>) {
+  chomp;
   if (/^q/i) { # q or quit for exit
     print "Goodbuy!\n";
     exit;
   }
 
   if (/^h/i) { # h or help for help
-    print "q or quit --- exit the script\n";
-    print "h or help --- print this help\n";
+    (my $message = <<"    END_MESSAGE") =~ s/^ {4}//gm;
+    q or quit --- exit the script
+    h or help --- print this help
+    - how to work with entire wishlist:
+    w update \$nickname --- will download wishlist from steam and compare results with old data if there is one
+    END_MESSAGE
+    print $message;
   }
+# work with entire wishlist
+  if (/^w /i) {
+    $_ =~ /w ([a-zA-Z]+)/;
+    my $marker = $1;
+    if ($marker eq 'update') {
+      $_ =~ /w (\w+)( (\S+))?/;
+      my $nickname = $3;
+      print "Try to load fresh wishlist for nickname [$nickname]\n";
+      WishList::load_wishlist($nickname);
+      WishList::parse_wishlist();
+    }
+  }
+
+  print '>> ';
 }
-#my $name=$ARGV[0];
-#WishList::load_wishlist($name);
-#WishList::parse_wishlist();
+
