e3b048cc2c5ff7fff46796b4a7591eb1b1cb5593
Takesha <graukin>
2014-12-26 22:17:07 +0300

    add a bit of interactivity

--- src/wishlist.pl
+++ src/wishlist.pl
@@ -2,6 +2,19 @@
 
 use WishList;
 
-my $name=$ARGV[0];
+while (1) {
+  print '>> ';
+  chomp($_ = <>);
+  if (/^q/i) { # q or quit for exit
+    print "Goodbuy!\n";
+    exit;
+  }
+
+  if (/^h/i) { # h or help for help
+    print "q or quit --- exit the script\n";
+    print "h or help --- print this help\n";
+  }
+}
+#my $name=$ARGV[0];
 #WishList::load_wishlist($name);
-WishList::parse_wishlist();
+#WishList::parse_wishlist();
