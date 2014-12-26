8d828130e5dc197c1959d937a1b815af460da9f4
Takesha <graukin>
2014-12-27 01:09:12 +0300

    parse discounts and prices; parse names; print map

--- src/WishList.pm
+++ src/WishList.pm
@@ -41,6 +41,7 @@ sub load_wishlist {
 
 sub parse_wishlist {
 # parse: <div class='wishlistRow ' id='game_$num'> -> http://store.steampowered.com/app/$num
+# <h4>$gameName</h4>
 # <div class='discount_pct'>-$d%</div>
 # <div class='discount_final_price'>$price
 # or
@@ -48,26 +49,63 @@ sub parse_wishlist {
   my $parser = HTML::TokeParser::Simple->new(file => $wishlistSrc);
   open (OUTFILE, '>', $wishlistIds);
   my $gameId=0;
+  my $gameName = '';
+  my $discount = 0;
+  my $price = 0;
   my %gameMap;
   while ( my $token = $parser->get_token ) { # get every token
     if ( $token->is_start_tag( 'div' ) ) { # pick only <div ...> from every row in wishlist
       my $divClass = $token->get_attr( 'class' );
       if ( length $divClass ) {
         if ( $divClass eq 'wishlistRow ' ) {
+          if ($gameId != 0) {
+            $gameMap{$gameId}->{'name'} = $gameName;
+            $gameMap{$gameId}->{'discount'} = $discount;
+            $gameMap{$gameId}->{'price'} = $price;
+            $gameName = '';
+            $discount = 0;
+            $price = 0;
+          }
           $gameId = $token->get_attr( 'id' );
           $gameId =~ s/game_//;
-          say $gameId;
         } elsif ( $divClass eq 'discount_pct' ) {
           # discount
-        } elsif ( $divClass eq 'discount_final_price' ) {
-          # price with discount
-        } elsif ( $divClass eq 'price' ) {
-          # just price
+          $token = $parser->get_token;
+          my $dText = $token->as_is;
+          $dText =~ /-(\d+)%/;
+          $discount = $1;
+        } elsif ( $divClass eq 'discount_final_price' or $divClass eq 'price' ) {
+          # price with discount or without
+          $token = $parser->get_token;
+          my $dToken = $token->as_is;
+          $dToken =~ /^(\d+)/g;
+          $price = $1;
         }
       }
+    } elsif ( $token->is_start_tag( 'h4' ) ) { # possibly it's name
+      $token = $parser->get_token;
+      $gameName = $token->as_is;
     }
   }
+  if ($gameId != 0) {
+    $gameMap{$gameId}->{'name'} = $gameName;
+    $gameMap{$gameId}->{'discount'} = $discount;
+    $gameMap{$gameId}->{'price'} = $price;
+  }
+  printMap(\%gameMap);
   close(OUTFILE);
+}
+
+##################### work with data map #########################
+sub printMap {
+  my ($mapRef) = @_;
+  while(my($k, $v) = each %$mapRef)
+  {
+    say $k;
+    say "  ".$v->{'name'};
+    say "  ".$v->{'price'};
+    say "  ".$v->{'discount'}."%" unless $v->{'discount'} == 0;
+  }
 }
 
 1;
